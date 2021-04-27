5e8f32c89b53fbf62b273cb351fa24a08c5fc819
Siehan <memain.ye.sylvie@gmail.com>
2021-04-27 08:41:17 +0200

    done

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,39 +1,27 @@
 const Todo = (props) => {
-  const { todo, deleteTodo, toggleCompleteTodo } = props
+  const { todo, deleteTodo, toggleCompleteTodo } = props;
   const style = {
-    textDecoration: todo.isCompleted ? "line-through" : "none"
-  }
+    textDecoration: todo.isCompleted ? "line-through" : "none",
+  };
   return (
-    <div className="shadow-sm border p-2 d-flex align-items-center justify-content-between mb-2">
+    <div className="shadow-sm border p-2 d-flex align-items-center justify-content-between mb-2 rounded-3">
       <span style={style}>{todo.text}</span>
       <div className="btn-group">
         {todo.isCompleted ? (
-          <button
-            className="btn btn-light btn-sm btn-dark"
-            type="button"
-            onClick={() => toggleCompleteTodo(todo)}
-          >
+          <button className="btn btn-light btn-sm btn-dark p-1" type="button" onClick={() => toggleCompleteTodo(todo)}>
             Rétablir
           </button>
         ) : (
-          <button
-            className="btn btn-light btn-sm"
-            type="button"
-            onClick={() => toggleCompleteTodo(todo)}
-          >
+          <button className="btn btn-light btn-sm p-1" type="button" onClick={() => toggleCompleteTodo(todo)}>
             Terminer
           </button>
         )}
-        <button
-          className="btn btn-danger btn-sm"
-          type="button"
-          onClick={() => deleteTodo(todo)}
-        >
+        <button className="btn btn-danger btn-sm p-1" type="button" onClick={() => deleteTodo(todo)}>
           Supprimer
         </button>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Todo
+export default Todo;
